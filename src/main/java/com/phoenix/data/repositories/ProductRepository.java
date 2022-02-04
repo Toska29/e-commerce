98dfa3147ef3fc3952596eeb967481b064c1a2cb
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-04 17:16:48 +0100

    repos

---
+++ src/main/java/com/phoenix/data/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.phoenix.data.repositories;
+
+import com.phoenix.data.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+}
