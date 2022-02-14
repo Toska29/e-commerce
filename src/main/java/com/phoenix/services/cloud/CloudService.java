01a736067613ad3f1cb8cd6aee6f48cc9fb1f30d
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-14 10:22:29 +0100

    cloudservices

---
+++ src/main/java/com/phoenix/services/cloud/CloudService.java
@@ -0,0 +1,14 @@
+package com.phoenix.services.cloud;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public interface CloudService {
+//    Map<?, ?> upload(File file, Map<?, ?> params) throws IOException;
+//    Map<?, ?> upload(MultipartFile file, Map<?, ?> params) throws IOException;
+    Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException;
+
+}
---
+++ src/main/java/com/phoenix/services/cloud/CloudinaryServiceImpl.java
@@ -0,0 +1,30 @@
+package com.phoenix.services.cloud;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
+@Service("cloudinary-service")
+public class CloudinaryServiceImpl implements CloudService {
+
+    @Autowired
+    Cloudinary cloudinary;
+
+//    @Override
+//    public Map<?, ?> upload(File file, Map<?, ?> params) throws IOException {
+//
+//        return cloudinary.uploader().upload(Files.readAllBytes(file.toPath()), params);
+//
+//    }
+
+    @Override
+    public Map<?, ?> upload(byte[] bytes, Map<?, ?> params) throws IOException {
+        return cloudinary.uploader().upload(bytes, params);
+    }
+}
