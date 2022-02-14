068a2346092d2aded2de8276fc517eee612d91f0
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-14 10:17:57 +0100

    Item model

---
+++ src/main/java/com/phoenix/data/models/Item.java
@@ -0,0 +1,35 @@
+package com.phoenix.data.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Product product;
+
+    private Integer quantityAdded;
+
+    public Item(Product product, Integer quantityAdded){
+        if(quantityAdded <= product.getQuantity()){
+            this.quantityAdded = quantityAdded;
+        }
+        this.product = product;
+    }
+
+    public void addQuantity(Integer quantityAdded){
+        if(quantityAdded <= product.getQuantity()){
+            this.quantityAdded = quantityAdded;
+        }
+    }
+}
