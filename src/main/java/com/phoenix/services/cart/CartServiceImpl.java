1e53ada230d85b63868d0a8139ea3e8791256928
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-16 13:43:34 +0100

    cart service Impl

---
+++ src/main/java/com/phoenix/services/cart/CartServiceImpl.java
@@ -0,0 +1,20 @@
+package com.phoenix.services.cart;
+
+import com.phoenix.data.dtos.CartDto;
+import com.phoenix.data.models.Cart;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl implements CartService {
+
+
+    @Override
+    public void saveCart(CartDto cartDto) {
+
+    }
+
+    @Override
+    public Cart viewCart() {
+        return null;
+    }
+}
