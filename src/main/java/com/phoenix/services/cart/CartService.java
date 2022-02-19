93c6ffcb10ba29cc4d3e989fafde73f824290eba
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-19 22:51:07 +0100

    cart service interface

--- src/main/java/com/phoenix/services/cart/CartService.java
+++ src/main/java/com/phoenix/services/cart/CartService.java
@@ -1,9 +1,16 @@
 package com.phoenix.services.cart;
 
-import com.phoenix.data.dtos.CartDto;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.phoenix.data.dtos.CartRequestDto;
+import com.phoenix.data.dtos.CartResponseDto;
+import com.phoenix.data.dtos.CartUpdateDto;
 import com.phoenix.data.models.Cart;
+import com.phoenix.web.exceptions.BusinessLogicException;
+import com.phoenix.web.exceptions.ProductDoesNotExistException;
+import com.phoenix.web.exceptions.UserNotFoundException;
 
 public interface CartService {
-    void saveCart(CartDto cartDto);
-    Cart viewCart();
+    CartResponseDto addItemToCart(CartRequestDto cartRequestDto) throws UserNotFoundException, ProductDoesNotExistException, BusinessLogicException;
+    CartResponseDto viewCart(Long userId) throws UserNotFoundException;
+    CartResponseDto updateCartItems(CartUpdateDto cartUpdateDto) throws UserNotFoundException, BusinessLogicException;
 }
