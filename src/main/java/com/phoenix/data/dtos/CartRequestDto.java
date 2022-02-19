5b1f971a7502d79eec6c84eaf64ba006de8ab177
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-02-19 22:47:27 +0100

    Cart request dto

--- src/main/java/com/phoenix/data/dtos/CartRequestDto.java
+++ src/main/java/com/phoenix/data/dtos/CartRequestDto.java
@@ -1,11 +1,12 @@
 package com.phoenix.data.dtos;
 
 import com.phoenix.data.models.Item;
+import lombok.Builder;
 import lombok.Data;
 
 
 @Data
-public class CartItemDto {
+public class CartRequestDto {
     private Long userId;
     private Long productId;
     private Integer quantity;
