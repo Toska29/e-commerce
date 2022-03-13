ea95fb22fd49e9fe7efff5303a1e5dc045cb9df4
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-03-13 07:20:09 +0100

    user detail service

---
+++ src/main/java/com/phoenix/security/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.phoenix.security;
+
+import com.phoenix.data.models.AppUser;
+import com.phoenix.data.models.Authority;
+import com.phoenix.data.repositories.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //query for user details from db
+        AppUser user = appUserRepository.findByEmail(username).orElse(null);
+
+        //check that user exists
+        if(user == null) throw new UsernameNotFoundException("User with email " + username + " not found");
+
+        //return userDetails
+        return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+
+    }
+
+    private List<SimpleGrantedAuthority> getAuthorities(List<Authority> authorities){
+        return authorities.stream().map(
+                authority -> {
+                    return new SimpleGrantedAuthority(authority.name());
+                }).collect(Collectors.toList());
+    }
+}
