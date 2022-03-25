72a55ddf9641fcd211ed0c5b475d594bd115fbb7
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2022-03-25 03:25:57 +0100

    product controller

--- src/main/java/com/phoenix/web/controllers/ProductRestController.java
+++ src/main/java/com/phoenix/web/controllers/ProductRestController.java
@@ -15,44 +15,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@ResponseBody()
+@RequestMapping("/api/products")
 public class ProductRestController {
     @Autowired
     ProductService productService;
 
     @GetMapping("")
-    public ResponseEntity<?> findAllProducts(){
+    public ResponseEntity<?> findAllProducts() {
         List<Product> products = productService.getAllProducts();
         return ResponseEntity.ok().body(products);
     }
-    @PostMapping("/newproducts")
-    public ResponseEntity<?> createProduct(@RequestBody ProductDto productDto){
+
+    @PostMapping(consumes = {"multipart/form-data"})
+    public ResponseEntity<?> createProduct(@ModelAttribute ProductDto productDto) {
+
         try {
             Product product = productService.createProduct(productDto);
             return ResponseEntity.ok().body(product);
-        }
-        catch (IllegalArgumentException | BusinessLogicException e){
-           return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (IllegalArgumentException | BusinessLogicException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-    @PatchMapping("/oldproducts/{productName}")
-    public ResponseEntity<?> updateProduct(@PathVariable String productName, @RequestBody ProductDto productDto){
+
+    @PatchMapping("/{productName}")
+    public ResponseEntity<?> updateProduct(@PathVariable String productName, @RequestBody ProductDto productDto) {
         try {
             Product product = productService.updateProduct(productName, productDto);
             return ResponseEntity.accepted().body(product);
-        }
-        catch (BusinessLogicException e){
+        } catch (BusinessLogicException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
-    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody JsonPatch productPatch){
-        try{
-           Product updatedProduct = productService.updateProductDetails(id, productPatch);
-           return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
-        }catch (BusinessLogicException | JsonPatchException | JsonProcessingException e){
-            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody JsonPatch productPatch) {
+        try {
+            Product updatedProduct = productService.updateProductDetails(id, productPatch);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+        } catch (BusinessLogicException | JsonPatchException | JsonProcessingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
 //        return null;
     }
